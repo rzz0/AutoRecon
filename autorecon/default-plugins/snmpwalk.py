696950cae550aabbbce00a66572533a0974e8765
rzz0 <rsrampazzo@gmail.com>
2025-11-16 14:02:37 -0300

    add snmpwalk extended

--- autorecon/default-plugins/snmpwalk.py
+++ autorecon/default-plugins/snmpwalk.py
@@ -14,6 +14,8 @@ class SNMPWalk(ServiceScan):
 
 	async def run(self, service):
 		await service.execute('snmpwalk -c public -v 1 {address} 2>&1', outfile='{protocol}_{port}_snmp_snmpwalk.txt')
+		await service.execute('snmpwalk -c public -v 1 {address} NET-SNMP-EXTEND-MIB::nsExtendObjects', outfile='{protocol}_{port}_snmp_snmpwalk_nsExtendObjects.txt')
+		await service.execute('snmpwalk -c public -v 1 {address} NET-SNMP-EXTEND-MIB::nsExtendOutputFull', outfile='{protocol}_{port}_snmp_snmpwalk_nsExtendOutputFull.txt')
 		await service.execute('snmpwalk -c public -v 1 {address} 1.3.6.1.2.1.25.1.6.0 2>&1', outfile='{protocol}_{port}_snmp_snmpwalk_system_processes.txt')
 		await service.execute('snmpwalk -c public -v 1 {address} 1.3.6.1.2.1.25.4.2.1.2 2>&1', outfile='{scandir}/{protocol}_{port}_snmp_snmpwalk_running_processes.txt')
 		await service.execute('snmpwalk -c public -v 1 {address} 1.3.6.1.2.1.25.4.2.1.4 2>&1', outfile='{protocol}_{port}_snmp_snmpwalk_process_paths.txt')
@@ -21,5 +23,3 @@ class SNMPWalk(ServiceScan):
 		await service.execute('snmpwalk -c public -v 1 {address} 1.3.6.1.2.1.25.2.3.1.4 2>&1', outfile='{protocol}_{port}_snmp_snmpwalk_software_names.txt')
 		await service.execute('snmpwalk -c public -v 1 {address} 1.3.6.1.4.1.77.1.2.25 2>&1', outfile='{protocol}_{port}_snmp_snmpwalk_user_accounts.txt')
 		await service.execute('snmpwalk -c public -v 1 {address} 1.3.6.1.2.1.6.13.1.3 2>&1', outfile='{protocol}_{port}_snmp_snmpwalk_tcp_ports.txt')
-		await service.execute('snmpwalk -c public -v 1 {address} NET-SNMP-EXTEND-MIB::nsExtendObjects 2>&1', outfile='{protocol}_{port}_snmp_snmpwalk_nsExtendObjects.txt')
-		await service.execute('snmpwalk -c public -v 1 {address} NET-SNMP-EXTEND-MIB::nsExtendOutputFull 2>&1', outfile='{protocol}_{port}_snmp_snmpwalk_nsExtendOutputFull.txt')
